8552b822be00ea3ac328317c512f06bad1c208d2
Jing-Ting Dong <stevendong2002@gmail.com>
2023-09-24 16:11:52 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,10 @@ function App() {
 
     return (
       <div className="App">
-        <h1>Hello World!</h1>
-        <h1>Welcome to my card</h1>
-        <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
+        <h1>Jing-Ting Dong at the Library</h1>
+        <img src="https://i.imgur.com/XPC2j0i.jpg" alt="Card Image" />
         <div className="paragraph">
-          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
+          <p>Hi my name is Jing-Ting Dong. This is an image of me giving a thumbs up at the library while working on this coding project. I like cabbage. I also like bacon. I really like eating but it gives me food coma. Bygolly, what a dilemma.</p>
         </div>
         <div class="btn-wrapper">
  <a href="https://hax.psu.edu">
